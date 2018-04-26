b87db4030479e961cd097ced726893195703956a
Dennis <deweis@gmx.ch>
2018-04-26 10:53:07 +0200

    Fix: Linter issues

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+/*jshint esversion: 6 */
 let currentPlayer = 1;    // 1 = Player 1, 2 = Player 2, 3 = computer
 let playMode = 2;         // 1 = 1 player, 2 = 2 players
-const strikes = ['c1 c2 c3','c4 c5 c6','c7 c8 c9','c1 c4 c7','c2 c5 c8','c3 c6 c9','c1 c5 c9','c3 c5 c7'];
+const strikes = ['c1 c2 c3', 'c4 c5 c6', 'c7 c8 c9', 'c1 c4 c7',
+                 'c2 c5 c8', 'c3 c6 c9', 'c1 c5 c9', 'c3 c5 c7'];
+const allCells = ['c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7', 'c8', 'c9'];
 let selected = [];
 let p1Score = [];
 let p2Score = [];
@@ -15,6 +18,7 @@ function play(id) {
   if (selected.includes(id)) {
     return;
   }
+
   if (currentPlayer === 1) {
     document.getElementById(id).innerHTML = p1Icon;
     document.getElementById(id).style.cursor = 'default';
@@ -50,15 +54,15 @@ function setPlayer() {
 function titleStyle() {
   if (playMode === 1) {
     if (currentPlayer === 3) {
-     document.getElementById("title").innerHTML = `Computer's turn..`;
-   } else {
-     document.getElementById("title").innerHTML = `Your turn..`;
-   }
+      document.getElementById('title').innerHTML = `Computer's turn..`;
+    } else {
+      document.getElementById('title').innerHTML = `Your turn..`;
+    }
+  } else {
+    document.getElementById('title').innerHTML = `Player ${currentPlayer} turn`;
   }
-  else {
-    document.getElementById("title").innerHTML = `Player ${currentPlayer} turn`;
-  }
-  document.getElementById("title").style.background = '#63ccff';
+
+  document.getElementById('title').style.background = '#63ccff';
 }
 
 /* Check if current move wins */
@@ -72,48 +76,53 @@ function checkScores() {
       if (checkItem.includes(item)) {
         counter++;
       }
+
       if (counter === 3) {
-        document.getElementById(checkItem[0]).style.background = "green";
-        document.getElementById(checkItem[1]).style.background = "green";
-        document.getElementById(checkItem[2]).style.background = "green";
-        document.getElementById("title").innerHTML = `Congrats!!`;
+        document.getElementById(checkItem[0]).style.background = 'green';
+        document.getElementById(checkItem[1]).style.background = 'green';
+        document.getElementById(checkItem[2]).style.background = 'green';
+        document.getElementById('title').innerHTML = `Congrats!!`;
         if (playMode === 2) {
-          document.getElementById("overlay-text").innerHTML = `Player ${currentPlayer} wins!`;
+          document.getElementById('overlay-text').innerHTML = `Player ${currentPlayer} wins!`;
           if (currentPlayer === 1) {
             p1OverallScore++;
           } else {
             p2OverallScore++;
           }
-        }
-        else {
+        } else {
           if (currentPlayer === 3) {
-            document.getElementById("overlay-text").innerHTML = `The Computer wins!`;
+            document.getElementById('overlay-text').innerHTML = `The Computer wins!`;
             p2OverallScore++;
           } else {
-              document.getElementById("overlay-text").innerHTML = `You win!`;
-              p1OverallScore++;
+            document.getElementById('overlay-text').innerHTML = `You win!`;
+            p1OverallScore++;
           }
         }
-        document.getElementById("overlay").style.display = "flex";
-        setTimeout(function(){
-          document.getElementById("overlay").style.display = "none";
+
+        document.getElementById('overlay').style.display = 'flex';
+        setTimeout(function () {
+          document.getElementById('overlay').style.display = 'none';
           clearPitch();
-        }, 2300);
+        }, 2300
+        );
         return;
       }
     }
+
     drawCheck--;
     if (selected.length === 9 && drawCheck === 0) {
-      document.getElementById("title").innerHTML = `Boring...`;
-      document.getElementById("overlay-text").innerHTML = `It's a draw!`;
-      document.getElementById("overlay").style.display = "flex";
-      setTimeout(function(){
-        document.getElementById("overlay").style.display = "none";
+      document.getElementById('title').innerHTML = `Boring...`;
+      document.getElementById('overlay-text').innerHTML = `It's a draw!`;
+      document.getElementById('overlay').style.display = 'flex';
+      setTimeout(function () {
+        document.getElementById('overlay').style.display = 'none';
         clearPitch();
-      }, 2300);
+      }, 2300
+      );
       return;
     }
   }
+
   setPlayer();
 }
 
@@ -125,83 +134,81 @@ function clearPitch() {
     } else {
       document.getElementById(item).innerHTML = '';
     }
-    document.getElementById(item).style.background = "white";
+
+    document.getElementById(item).style.background = 'white';
   }
+
   currentPlayer = 1;
   selected = [];
   p1Score = [];
   p2Score = [];
   titleStyle();
-  let cells = document.getElementsByClassName("cell");
+  let cells = document.getElementsByClassName('cell');
   for (const cell of cells) {
-    cell.style.cursor = "pointer";
+    cell.style.cursor = 'pointer';
   }
-  document.getElementById("player1-stats").innerHTML = p1OverallScore;
-  document.getElementById("player2-stats").innerHTML = p2OverallScore;
+
+  document.getElementById('player1-stats').innerHTML = p1OverallScore;
+  document.getElementById('player2-stats').innerHTML = p2OverallScore;
 }
 
 function ePlayer() {
-  const allCells = ['c1','c2','c3','c4','c5','c6','c7','c8','c9'];
-  const freeCells = allCells.filter( x => !selected.includes(x) );
-  const selection = freeCells[Math.floor(Math.random() * (freeCells.length-1 - 0 + 0)) + 0];
+  const freeCells = allCells.filter(x => !selected.includes(x));
+  const selection = freeCells[Math.floor(Math.random() * (freeCells.length - 1)) + 0];
   titleStyle();
-  setTimeout(function(){
+  setTimeout(function () {
     play(selection);
   }, 200);
 }
 
-document.getElementById("btn-1player").addEventListener("click", function() {
+document.getElementById('btn-1player').addEventListener('click', function () {
   playMode = 1;
-  document.getElementById("btn-1player").style.display = "none";
-  document.getElementById("btn-2players").style.display = "none";
-  document.getElementById("title").innerHTML = "Would you like to be X or O?";
-  document.getElementById("btn-x").style.display = "inline";
-  document.getElementById("btn-o").style.display = "inline";
+  document.getElementById('btn-1player').style.display = 'none';
+  document.getElementById('btn-2players').style.display = 'none';
+  document.getElementById('title').innerHTML = 'Would you like to be X or O?';
+  document.getElementById('btn-x').style.display = 'inline';
+  document.getElementById('btn-o').style.display = 'inline';
   p2Name = 'Computer';
 });
 
-document.getElementById("btn-2players").addEventListener("click", function() {
+document.getElementById('btn-2players').addEventListener('click', function () {
   playMode = 2;
-  document.getElementById("btn-1player").style.display = "none";
-  document.getElementById("btn-2players").style.display = "none";
-  document.getElementById("title").innerHTML = "Player 1: Would you like to be X or O?";
-  document.getElementById("btn-x").style.display = "inline";
-  document.getElementById("btn-o").style.display = "inline";
+  document.getElementById('btn-1player').style.display = 'none';
+  document.getElementById('btn-2players').style.display = 'none';
+  document.getElementById('title').innerHTML = 'Player 1: Would you like to be X or O?';
+  document.getElementById('btn-x').style.display = 'inline';
+  document.getElementById('btn-o').style.display = 'inline';
   p2Name = 'Player 2';
 });
 
-document.getElementById("btn-x").addEventListener("click", function() {
+document.getElementById('btn-x').addEventListener('click', function () {
   p1Icon = 'x';
   p2Icon = 'o';
-  document.getElementById("btn-x").style.display = "none";
-  document.getElementById("btn-o").style.display = "none";
-  document.getElementById("main").style.display = "flex";
-  document.getElementById("bottom-section").style.display = "flex";
-  document.getElementById("player2-name").innerHTML = p2Name;
+  document.getElementById('btn-x').style.display = 'none';
+  document.getElementById('btn-o').style.display = 'none';
+  document.getElementById('main').style.display = 'flex';
+  document.getElementById('bottom-section').style.display = 'flex';
+  document.getElementById('player2-name').innerHTML = p2Name;
   titleStyle();
 });
 
-document.getElementById("btn-o").addEventListener("click", function() {
+document.getElementById('btn-o').addEventListener('click', function () {
   p1Icon = 'o';
   p2Icon = 'x';
-  document.getElementById("btn-x").style.display = "none";
-  document.getElementById("btn-o").style.display = "none";
-  document.getElementById("main").style.display = "flex";
-  document.getElementById("bottom-section").style.display = "flex";
-  document.getElementById("player2-name").innerHTML = p2Name;
+  document.getElementById('btn-x').style.display = 'none';
+  document.getElementById('btn-o').style.display = 'none';
+  document.getElementById('main').style.display = 'flex';
+  document.getElementById('bottom-section').style.display = 'flex';
+  document.getElementById('player2-name').innerHTML = p2Name;
   titleStyle();
 });
 
-document.getElementById("btn-reset").addEventListener("click", function() {
+document.getElementById('btn-reset').addEventListener('click', function () {
   location.reload();
 });
 
-document.getElementById("c1").addEventListener("click", function() {play('c1')});
-document.getElementById("c2").addEventListener("click", function() {play('c2')});
-document.getElementById("c3").addEventListener("click", function() {play('c3')});
-document.getElementById("c4").addEventListener("click", function() {play('c4')});
-document.getElementById("c5").addEventListener("click", function() {play('c5')});
-document.getElementById("c6").addEventListener("click", function() {play('c6')});
-document.getElementById("c7").addEventListener("click", function() {play('c7')});
-document.getElementById("c8").addEventListener("click", function() {play('c8')});
-document.getElementById("c9").addEventListener("click", function() {play('c9')});
+for (const cell of allCells) {
+  document.getElementById(cell).addEventListener('click', function () {
+    play(cell);
+  });
+}
